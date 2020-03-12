c27946f42e9b058e64bddc0c1ca4bcc10175c1ac
Peter Szabo <pts@fazekas.hu>
2020-03-13 00:13:04 +0100

    added detection of format=lz4 and format=lz4-legacy

--- mediafileinfo_detect.py
+++ mediafileinfo_detect.py
@@ -6695,6 +6695,9 @@ FORMAT_ITEMS = (
     ('zstd', (0, ('\x25\xb5\x2f\xfd', '\x28\xb5\x2f\xfd'))),
     # http://fileformats.archiveteam.org/wiki/Zstandard_dictionary
     ('zstd-dict', (0, '\x37\xa4\x30\xec')),
+    # http://fileformats.archiveteam.org/wiki/LZ4
+    ('lz4', (0, '\x04\x22\x4d\x18')),
+    ('lz4-legacy', (0, '\x04\x22\x4d\x18')),
 
     # Non-compressed, non-media.
 
