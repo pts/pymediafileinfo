0659b5fb61f22e2085c36be0b16df2a6eea6c956
Peter Szabo <pts@fazekas.hu>
2020-03-13 14:05:45 +0100

    added detection of format=com32r

--- mediafileinfo_detect.py
+++ mediafileinfo_detect.py
@@ -6935,6 +6935,8 @@ FORMAT_ITEMS = (
     ('exe', (0, 'MZ', 64, lambda header: (len(header) >= 64, 1))),
     ('dotnetexe',),  # From 'exe'.
     ('winexe',),  # From 'exe'.
+    # https://wiki.syslinux.org/wiki/index.php?title=Doc/comboot#COM32R_file_format
+    ('com32r', (0, '\xb8\xfeL\xcd!')),  # .c32
     # https://github.com/pts/pts-xcom
     ('xcom', (0, "&XPZ,2P_0E[0E_,pP[,Eu\r\nR^!5+1+1CC+1)5GGHu#PWtl6~!ugH\"!rE\"!~~0B(m\"!4r!!Y~!)E~\"0~~Cump!!|d\r\n~E)!~~0B(m\"!pq!\"G0!!oD!\"B~\"v_Q\"! PSW\r\n")),
     # http://nozdr.ru/marinais/
