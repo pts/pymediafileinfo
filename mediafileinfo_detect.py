c48f7fe496deb23b989cf7bcfde2c7ed89afcb25
Peter Szabo <pts@fazekas.hu>
2021-11-29 00:22:43 +0100

    added acodec=eac3 detection for format=mkv

--- mediafileinfo_detect.py
+++ mediafileinfo_detect.py
@@ -408,7 +408,8 @@ MKV_CODEC_IDS = {
     'A_PCM/INT/LIT': 'pcm',
     'A_PCM/FLOAT/IEEE': 'pcm',
     'A_MPC': 'mpc',
-    'A_AC3': 'ac3',
+    'A_AC3': 'ac3',   # ATSC A/52a https://wiki.multimedia.cx/index.php/A52
+    'A_EC3': 'eac3',  # ATSC A/52b https://wiki.multimedia.cx/index.php/A52
     'A_ALAC': 'alac',
     'A_DTS': 'dts',
     'A_DTS/EXPRESS': 'dts-express',
